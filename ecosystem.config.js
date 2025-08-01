de8c59d6bd898bf2d2c88e7459a58152ead0103f
damencho <damencho@jitsi.org>
2025-08-01 19:01:34 +0300

    Fixes reading env variables.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,12 +4,6 @@ module.exports = {
     script: './dist/index.js',
     instances: 1,
     exec_mode: 'fork',
-    env_production: {
-      NODE_ENV: 'production',
-      PORT: 3000,
-      JWT_PUBLIC_KEY_PATH: process.env.JWT_PUBLIC_KEY_PATH || '/etc/ssl/certs/jwt-public-key.pem',
-      BASE_URL: process.env.BASE_URL || 'http://localhost:3000'
-    },
     log_file: './logs/app.log',
     out_file: './logs/out.log',
     error_file: './logs/error.log',
